74c633949c5468f23cb67a475a96a79a4d4fac8f
Yagoi23 <jeschollum@gmail.com>
2021-05-07 11:54:06 +1200

    trying to make shit work

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -14,3 +14,12 @@ func _physics_process(delta):
 	if Input.action_press("move_left"):
 		move_dir -= 1
 	move_and_slide(Vector2(move_dir * move_speed, y_vel), Vector2(0,-1))
+	
+	var on_ground = is_on_floor()
+	y_vel += gravity
+	if on_ground and Input.action_press("jump"):
+		y_vel -= jump_force
+	if on_ground and y_vel >= 5:
+		y_vel = 5
+	if y_vel > max_fall_speed:
+		y_vel = max_fall_speed 
