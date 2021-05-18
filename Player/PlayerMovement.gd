38af095ed52f2fd13be04fc82f520decee2a2d8a
Yagoi23 <jeschollum@gmail.com>
2021-05-18 13:22:45 +1200

    trying to make water

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -10,9 +10,20 @@ const MAX_MOVE_SPEED = 1000
 var MOVE_SPEED = 0
 
 var y_velo = 0
+var move_dir = 0
+
+var IN_WATER = false
 
 func _physics_process(delta):
-	var move_dir = 0
+	move_dir = 0
+	if IN_WATER == true:
+		y_velo += JUMP_FORCE
+		move_dir = 10
+		move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo),Vector2(0, -1))
+	else:
+		move_normal()
+	
+func move_normal():
 	if Input.is_action_pressed("move_right"):
 		move_dir += 1
 	if Input.is_action_pressed("move_left"):
@@ -38,3 +49,13 @@ func _physics_process(delta):
 	if MOVE_SPEED > MAX_MOVE_SPEED:
 		MOVE_SPEED = MAX_MOVE_SPEED
 
+
+
+
+
+
+func _on_Area2D_area_entered(area, body):
+	if body.is_in_group("Player"):
+		IN_WATER = true
+	else:
+		IN_WATER = false
