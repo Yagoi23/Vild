1554c23033e71079fb49ad65763b8ed5f55a950d
Yagoi23 <jeschollum@gmail.com>
2021-06-02 11:58:39 +1200

    movement work

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -4,42 +4,75 @@ export (int) var speed = 120
 export (int) var jump_speed = -180
 export (int) var gravity = 400
 
+export (int) var water_gravity = 10
+export (int) var water_jump_speed = -80
+export (int) var water_speed = 60
+
 var velocity = Vector2.ZERO
 
-export (float,0,1,0) var friction = 0.1
-export (float,0,1,0) var acceleration = 0.25
+export (float,0,1,0) var friction = 10
+export (float,0,1,0) var acceleration = 25
+
+export (float,0,1,0) var water_acceleration = 5
+export (float,0,1,0) var water_friction = 100
 
 enum state {RUNNING, JUMP, IDLE, SWIMMING, FALL}
 
 var player_state = state.IDLE
 
+var in_water = false
+
 func get_input():
-	var move_dir = 0
-	if Input.is_action_pressed("move_right"):
-		move_dir += 1
-	if Input.is_action_pressed("move_left"):
-		move_dir -= 1
-	if move_dir != 0:
-		velocity.x = lerp(velocity.x, move_dir * speed, acceleration)
-	else:
-		velocity.x = lerp(velocity.x, 0, friction)
+	var move_dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	if in_water == false:
+		if move_dir != 0:
+			velocity.x = move_toward(velocity.x,move_dir*speed,acceleration)
+		else:
+			velocity.x = move_toward(velocity.x, 0, friction)
+	elif in_water == true:
+		if move_dir != 0:
+			velocity.x = move_toward(velocity.x,move_dir*water_speed,water_acceleration)
+		else:
+			velocity.x = move_toward(velocity.x, 0, water_friction)
 
 func _physics_process(delta):
-	print(velocity)
-	if velocity.x == 0:
-		player_state = state.IDLE
-	elif velocity.x != 0:
-		player_state = state.IDLE
-	
-	if is_on_floor():
-		if Input.is_action_just_pressed("jump"):
-			velocity.y = jump_speed
+	#print(velocity)
+	get_input()
+	if in_water == false:
+		if velocity.x == 0:
+			player_state = state.IDLE
+		elif velocity.x != 0:
+			player_state = state.RUNNING
+		
+		if is_on_floor():
+			if Input.is_action_just_pressed("jump"):
+				velocity.y = jump_speed
+				player_state = state.JUMP
+		
+		if velocity.y < 0:
 			player_state = state.JUMP
+		else:
+			player_state = state.FALL
+		
+		velocity.y += gravity * delta
+		velocity = move_and_slide(velocity, Vector2.UP)
+	elif in_water == true:
+		if Input.is_action_just_pressed("jump"):
+				velocity.y = water_jump_speed
+		velocity.y += water_gravity * delta
+		velocity = move_and_slide(velocity, Vector2.UP)
 	
-	if velocity.y < 0:
-		player_state = state.JUMP
-	else:
-		player_state = state.FALL
-	
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
+
+func hit_player(ammount,dir):
+	PlayerStats.Health -= ammount
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("Water"):
+		print("water") # Replace with function body.
+		in_water = true
+
+
+func _on_Area2D_area_exited(area):
+	if area.is_in_group("Water"):
+		print("exit water") # Replace with function body.
+		in_water = false
