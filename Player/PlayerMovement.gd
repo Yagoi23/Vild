548b675d57ba839d9f0d60bf892172cdcae20fce
Yagoi23 <jeschollum@gmail.com>
2021-08-13 10:54:45 +1200

    ....

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -53,6 +53,9 @@ func get_input():
 		$Sprite.flip_h = true
 	elif Input.is_action_pressed("move_right"):
 		$Sprite.flip_h = false
+#attack
+	if Input.is_action_pressed("attack"):
+		pass
 
 func _process(delta):
 	if Input.is_action_just_pressed("EnemySense"):
