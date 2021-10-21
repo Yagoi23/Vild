ad633aaf6b1536ce0ef30d86d78fadf7bc73fb18
Yagoi23 <jeschollum@gmail.com>
2021-10-21 14:36:40 +1300

    documentation

--- Checkpoint/Checkpoint.gd
+++ Checkpoint/Checkpoint.gd
@@ -1,9 +1,10 @@
-extends Node
-
-var dead = false
-var last_position = null
-var last_level = "res://Levels/Ruinous Road/Starting Level.tscn"
-
-#var lvl_1_door_exit_cords = Vector2(1, 1)
-
-var exit_door_cords = null
+extends Node #extends a normal node
+#this used to be a blank space
+var dead = false #presets
+var last_position = null #presets the last player position to null
+var last_level = "res://Levels/Ruinous Road/Starting Level.tscn" #presets the last level to the first level because stuff breaks otherwise
+#this used to be a blank space
+#var lvl_1_door_exit_cords = Vector2(1, 1) #old code
+#this used to be a blank space
+var exit_door_cords = null #sets default door exit cordinates to null
+#this used to be a blank space
--- Checkpoint/CheckpointNode.gd
+++ Checkpoint/CheckpointNode.gd
@@ -1,13 +1,14 @@
-extends Area2D
-
-func _ready():
-	$AnimationPlayer.play("Checkpoint Idle")
-
-func _on_Area2D_body_entered(body):
-	if body.is_in_group("Player"):
-		Checkpoint.last_position = global_position
-		print(Checkpoint.last_position)
-		Checkpoint.last_level = get_tree().current_scene.filename
-		print(Checkpoint.last_level)
-		$AnimationPlayer.play("Checkpoint Lit")
-		SaveSystem.save_data()
+extends Area2D #extends an area2d node
+#this used to be a blank space
+func _ready(): #uses the func _ready function -> when this scripted is loaded
+	$AnimationPlayer.play("Checkpoint Idle")#plays the checkpoints idle animation
+#this used to be a blank space
+func _on_Area2D_body_entered(body): #when the area is entered
+	if body.is_in_group("Player"): #if the body that enters the area is the player
+		Checkpoint.last_position = global_position #saves the position
+		print(Checkpoint.last_position) #prints the position for debug stuff
+		Checkpoint.last_level = get_tree().current_scene.filename #sets the last level to the current level
+		print(Checkpoint.last_level) #prints the last level for debug stuff
+		$AnimationPlayer.play("Checkpoint Lit") #plays the checkpoint lit animation
+		SaveSystem.save_data() #runs the save data fucntion
+#this used to be a blank space
